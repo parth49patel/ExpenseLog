d8fab1c110099c49bfa44a47a45ea4632577aec6
Parth Patel <4parth9@gmail.com>
2025-01-25 18:29:43 -0500

    Initial Commit

---
+++ ExpenseLog/ExpenseLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseLogApp.swift
+//  ExpenseLog
+//
+//  Created by Parth Patel on 2025-01-25.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
